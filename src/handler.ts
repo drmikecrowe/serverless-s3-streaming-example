a49421a3962a0110d889f790a06956d146049bfe
Mike Crowe <drmikecrowe@gmail.com>
2019-09-25 12:51:27 -0400

    Adding new lambda logging

--- src/handler.ts
+++ src/handler.ts
@@ -1,23 +1,25 @@
 import { S3CreateEvent, S3EventRecord } from "aws-lambda";
 import "source-map-support/register";
 import dalamb from "dalamb";
-import { FileHandler, IFileHandler, IOutputFile } from "../src/lib/FileHandler";
-import { getReadStream, getWriteStream } from './lib/StreamUtils.live';
+import { FileHandler } from "../src/lib/FileHandler";
+
+const log = require("lambda-log");
 
 export default dalamb<S3CreateEvent>(async event => {
+    // set some optional metadata to be included in all logs (this is an overkill example)
+    log.options.meta.event = event;
+    log.options.debug = (process.env.STAGE === "dev");
+    // add additional tags to all logs
+    log.options.tags.push(...[process.env.STAGE, event.Records[0].s3.bucket]);
     const promises: Promise<void>[] = [];
     event.Records.forEach((record: S3EventRecord) => {
+        const bucket = record.s3.bucket.name;
         const filename = record.s3.object.key;
         const filesize = record.s3.object.size;
-        console.log(`New file has been uploaded: ${filename} (${filesize} bytes)`);
-        const fileHandlerSetup: IFileHandler = {
-            s3Bucket: record.s3.bucket.name,
-            s3Key: filename,
-            getReadStream,
-            getWriteStream,
-        };
-        const fileHandler: FileHandler = new FileHandler(fileHandlerSetup);
+        log.info(`New file has been uploaded: ${filename} (${filesize} bytes)`);
+        const fileHandler: FileHandler = new FileHandler(bucket, filename, log);
         promises.push(fileHandler.Process());
     });
-    return Promise.all(promises);
+    await Promise.all(promises);
+    log.info(`Processed ${promises.length} files`);
 });
